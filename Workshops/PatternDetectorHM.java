55a7d5665474855115ba662292cd5dee04cd7e08
soulsdevyergit config --global user.namegit config --global user.name soulsdevyergit config --global user.name soulsdevyer <marcellayermain@gmail.com>
2025-03-10 16:52:09 -0500

    add PatternDetectorHM

---
+++ Workshops/PatternDetectorHM.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class PatternDetectorHM {
+    public static void main(String[] args) {
+        int[] numbers = new int[10];
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        scanner.close();
+        
+        int[] result = detectPattern(numbers);
+        System.out.println("Arreglo original: " + Arrays.toString(numbers));
+        
+
+        System.out.println("\nArreglo nuevo: " + Arrays.toString(result));
+        
+        
+    }
+
+    public static int[] detectPattern(int[] array) {
+        int[] newArray = new int[(array.length - 1) * 2];
+        int newIndex = 0;
+       
+        
+        for (int i = 0; i < array.length - 1; i++) {
+            newArray[newIndex++] = array[i];
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            newArray[newIndex++] = array[i];
+        }
+        
+        
+   
+        while (newIndex < newArray.length) {
+            newArray[newIndex++] = 0;
+        }
+        
+        return newArray;
+    }
+}
